4888644cdfef29b223119caa491ffb920cfc9ac8
TheFlash98 <mastermind.sarthak@gmail.com>
2019-10-20 14:13:00 +0530

    UI is fun

--- ui/main.py
+++ ui/main.py
@@ -23,16 +23,22 @@ class CardSprite(pg.sprite.Sprite):
         self.name = card.name
         self.x = int(x)
         self.y = int(y)
-        self.card_active = False
+        self.card_active = True
         self.front_image = card.image
         self.back_image = 'card_back.png'
+        self.selected_image = 'selected.png'
         self.played = False
+        self.highlight = False
 
     def draw(self, gamedisplay):
         if not self.played:
             if self.card_active:
-                self.image = pg.image.load(str(self.front_image))
-                self.image = pg.transform.scale(self.image, (100, 150))
+                if self.highlight:
+                    self.image = pg.image.load(str(self.selected_image))
+                    self.image = pg.transform.scale(self.image, (100, 150))
+                else:
+                    self.image = pg.image.load(str(self.front_image))
+                    self.image = pg.transform.scale(self.image, (100, 150))
                 gamedisplay.blit(self.image, (self.x, self.y))
                 display_text(self.name, self.x + 20, self.y +10, gamedisplay)
                 display_text("Attack: " + str(self.attack), self.x + 40, self.y +100, gamedisplay)
@@ -73,9 +79,9 @@ def main():
     card2=Card("new1", "blank.png", 20,20)
     card3 = Card("new2", "blank.png", 30, 30)
     
-    card1_sprite = CardSprite(card1, 300, 300)
-    card2_sprite = CardSprite(card2, 190, 300)
-    card3_sprite = CardSprite(card3, 410, 300)
+    # card1_sprite = CardSprite(card1, 300, 300)
+    # card2_sprite = CardSprite(card2, 190, 300)
+    # card3_sprite = CardSprite(card3, 410, 300)
 
     attack_button = pg.Rect(190, 460, 100, 20)
     attack_text = myfont.render('ATTACK', False, (255, 255, 255))
@@ -84,11 +90,66 @@ def main():
     defense_button = pg.Rect(410, 460, 100, 20)
     defense_text = myfont.render('DEFENSE', False, (255, 255, 255))
     defense_color = red_color
+
+    join_button = pg.Rect(300, 460, 100, 20)
+    join_text = myfont.render('JOIN', False, (255, 255, 255))
+    join_color = red_color
+
+    all_cards = []
+    for i in range(0,6):
+        all_cards.append(card1)
+    all_card_sprites = []
+    selected_cards = []
+    start_x = 190
+    start_y = 50
+    i = 0
+    for card in all_cards:
+        if i < 3:
+            all_card_sprites.append(CardSprite(card, start_x + i*110, start_y))
+        else:
+            all_card_sprites.append(CardSprite(card, start_x + (i-3)*110, start_y + 200))
+        i = i + 1
+        
     while not done:
         events = pg.event.get()
         for event in events:
             if event.type == pg.QUIT :
                 done = True
+        if game_state == "lobby":
+            for event in events:
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    mouse_pos = event.pos
+                    for card_sprite in all_card_sprites:
+                        if card_sprite.x < mouse_pos[0] < card_sprite.x + 100 and card_sprite.y < mouse_pos[1] < card_sprite.y + 150:
+                            card_sprite.highlight = not card_sprite.highlight
+
+                    if join_button.collidepoint(mouse_pos):
+                        for card_sprite in all_card_sprites:
+                                if card_sprite.highlight:
+                                    selected_cards.append(card_sprite)
+                        if(len(selected_cards)==3):
+                            card1_sprite = selected_cards[0]
+                            card1_sprite.x = 190
+                            card1_sprite.y = 300
+                            card1_sprite.highlight = False
+                            card2_sprite = selected_cards[1]
+                            card2_sprite.x = 300
+                            card2_sprite.y = 300
+                            card2_sprite.highlight = False
+                            card3_sprite = selected_cards[2]
+                            card3_sprite.x = 410
+                            card3_sprite.y = 300
+                            card3_sprite.highlight = False
+                            game_state = "display_cards"
+                        else:
+                            selected_cards = []
+                        print(selected_cards)
+            screen.fill((30, 30, 30))
+            for card_sprite in all_card_sprites:
+                card_sprite.draw(screen)
+            pg.draw.rect(screen, defense_color, join_button, 0)
+            screen.blit(join_text, (join_button.x + 20, join_button.y))
+
         if game_state == "get_hash":
             for event in events:
                 if event.type == pg.QUIT:
@@ -109,12 +170,12 @@ def main():
                         print('button was pressed at {0}'.format(mouse_pos))
                         button_color = bright_green
                         player_hash = text
-                        game_state = "display_cards"
+                        game_state = "lobby"
                 if event.type == pg.KEYDOWN:
                     if active:
                         if event.key == pg.K_RETURN:
                             player_hash = text
-                            game_state = "display_cards"
+                            game_state = "lobby"
                         elif event.key == pg.K_BACKSPACE:
                             text = text[:-1]
                         else:
